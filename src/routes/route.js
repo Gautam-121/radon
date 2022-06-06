cceb423a888275e12b6765606f7330b37347aa20
Gautam-121 <gautamdhakate1234@gmail.com>
2022-06-06 22:02:08 +0530

    query param

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,47 +1,83 @@
 const express = require('express');
-const myHelper = require('../util/helper')
-const underscore = require('underscore')
+
 
 const router = express.Router();
 
-router.get('/test-me', function (req, res) {
-    myHelper.printDate()
-    myHelper.getCurrentMonth()
-    myHelper.getCohortData()
-    let firstElement = underscore.first(['Sabiha','Akash','Pritesh'])
-    console.log('The first element received from underscope function is '+firstElement)
-    res.send('My first ever api!')
-});
-
-router.get('/hello', function (req, res) {
-   
-    res.send('Hello there!')
-});
-
-router.get('/candidates', function(req, res){
-    console.log('Query paramters for this request are '+JSON.stringify(req.query))
-    let gender = req.query.gender
-    let state = req.query.state
-    let district = req.query.district
-    console.log('State is '+state)
-    console.log('Gender is '+gender)
-    console.log('District is '+district)
-    let candidates = ['Akash','Suman']
-    res.send(candidates)
-})
-
-router.get('/candidates/:canidatesName', function(req, res){
-    console.log('The request objects is '+ JSON.stringify(req.params))
-    console.log('Candidates name is '+req.params.canidatesName)
-    res.send('Done')
-})
-
 router.get('/movie', function(req,res){
-
-    const array = ["Dhol","Golmaal","Raja babu","Spiderman"]
-
+    const array =["Dhol","Golmaal","Raja babu","spiderman"];
     res.send(array)
 })
+
+router.get('/movie/:indexNumber', function(req, res){
+
+    let array = ["Sholey","Bhagam-bhag","Spiderman","Raja babu"];
+
+   
+    const array1 = req.params.indexNumber
+
+    if(array1<=3){
+        res.send(array[array1])
+    }else{
+        res.send(" use a valid index")
+    }
+    
+    
+   
+})
+
+router.get('/films', function(req,res){
+
+    const array = [ {
+        id: 1,
+        Name: 'The Shining'
+       }, {
+        id : 2,
+        Name: 'Incendies'
+       }, {
+        id: 3,
+        name: 'Rang de Basanti'
+       }, {
+        id: 4,
+        name: 'Finding Nemo'
+       }]
+
+       res.send(array)
+
+
+       
+})
+
+router.get('/films/:filmid', function(req,res){
+    const array =  [ {
+        id: 1,
+        Name: 'The Shining'
+       }, {
+        id : 2,
+        Name: 'Incendies'
+       }, {
+        id: 3,
+        name: 'Rang de Basanti'
+       },{
+        id: 4,
+        name: 'Finding Nemo'
+       }];
+
+       const array1 = req.params.filmid
+
+      if(array1<=array.length){
+        res.send(array[array1])
+      }else{
+        res.send("No movie exists with this id")
+
+
+      }
+
+})
+
+
+
+
+
 
 module.exports = router;
 // adding this comment for no reason
